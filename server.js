cd6384e5180cf4b12907e1c1911080214896968c
jephthe <jephthepetitfrere@gmail.com>
2024-10-01 19:56:51 -0400

    serer.js update

--- server.js
+++ server.js
@@ -8,13 +8,17 @@ const methodOverride = require("method-override");
 const morgan = require("morgan");
 
 // Set the port from environment variable or default to 3000
-const port = process.env.PORT ? process.env.PORT : "3000";
+const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+
+mongoose.connection.on("error", (err) => {
+  console.log(err)
+})
 
 // Middleware to parse URL-encoded data from forms
 app.use(express.urlencoded({ extended: false }));
